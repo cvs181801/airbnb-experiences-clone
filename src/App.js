676d5b7ffc67d0f639213ada6b332bdb63f4fe98
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-11-27 10:08:56 -0800

    add more featured cards with images

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Featured from './components/Featured.js'
 import featuredSwim from './components/image_12.png'
 import featuredWedding from './components/wedding-photography 1.png'
 import featuredBike from './components/mountain-bike 1.png'
+import featuredYoga from './components/yoga.jpeg'
+import featuredVenice from './components/venice.jpeg'
+import featuredEiffel from './components/eiffeltower.jpeg'
 
 function App() {
   return (
@@ -45,7 +48,37 @@ function App() {
               country="USA"
               summary="Group mountain biking"
               startingPrice="From $50"
-        />
+          />
+          <Featured
+              src={featuredYoga}
+              alt="featured Air BnB Experiences Yoga"
+              className="featuredYoga"
+              rating="4.9"
+              numOfRatings="(22)"
+              country="Austrailia"
+              summary="Yoga with Sari"
+              startingPrice="From $12"
+          />
+          <Featured
+              src={featuredVenice}
+              alt="featured Air BnB Experiences Venice"
+              className="featuredVenice"
+              rating="5"
+              numOfRatings="(249)"
+              country="Italy"
+              summary="Virtual gondola ride"
+              startingPrice="From $17"
+          />
+          <Featured
+              src={featuredEiffel}
+              alt="featured Air BnB Experiences Eiffel Tower"
+              className="featuredEiffel"
+              rating="3.5"
+              numOfRatings="(81)"
+              country="France"
+              summary="La Tour Eiffel holiday lights tour"
+              startingPrice="From $20"
+          />
       </div>
     </div>
     
