3b6a0e8a1b8e07c9c75137a25297cdb4ad72df15
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-11-30 10:41:47 -0800

    add poppins font

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.css';
+import './index.css';
 import Navbar from './components/Navbar';
 import Featured from './components/Featured.js'
-import featuredSwim from './components/image_12.png'
-import featuredWedding from './components/wedding-photography 1.png'
-import featuredBike from './components/mountain-bike 1.png'
-import featuredYoga from './components/yoga.jpeg'
-import featuredVenice from './components/venice.jpeg'
-import featuredEiffel from './components/eiffeltower.jpeg'
 import cardsData from './components/cardsData'
 
 
@@ -33,60 +28,7 @@ function App() {
         <div className="featuredContainer">
 
           {featuredCards}
-          {/* <Featured
-              src={featuredSwim}
-              alt="featured Air BnB Experiences Swim"
-              rating="5.0"
-              numOfRatings="(6)"
-              country="USA"
-              summary="Life Lessons with Katie Zaferes"
-              startingPrice="From $136"
-          />
-          <Featured
-              src={featuredWedding}
-              alt="featured Air BnB Experiences Wedding"
-              rating="5.0"
-              numOfRatings="(30)"
-              country="USA"
-              summary="Learn wedding photography"
-              startingPrice="From $125"
-          />
-          <Featured
-              src={featuredBike}
-              alt="featured Air BnB Experiences Bike"
-              rating="4.8"
-              numOfRatings="(2)"
-              country="USA"
-              summary="Group mountain biking"
-              startingPrice="From $50"
-          />
-          <Featured
-              src={featuredYoga}
-              alt="featured Air BnB Experiences Yoga"
-              rating="4.9"
-              numOfRatings="(22)"
-              country="Australia"
-              summary="Yoga with Sari"
-              startingPrice="From $12"
-          />
-          <Featured
-              src={featuredVenice}
-              alt="featured Air BnB Experiences Venice"
-              rating="5"
-              numOfRatings="(249)"
-              country="Italy"
-              summary="Virtual gondola ride"
-              startingPrice="From $17"
-          />
-          <Featured
-              src={featuredEiffel}
-              alt="featured Air BnB Experiences Eiffel Tower"
-              rating="3.5"
-              numOfRatings="(81)"
-              country="France"
-              summary="La Tour Eiffel holiday lights tour"
-              startingPrice="From $20"
-          /> */}
+         
       </div>
     </div>
     
